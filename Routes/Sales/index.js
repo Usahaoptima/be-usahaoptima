914728e922583d9551dce28b91b37b34c6afb23b
Wutdehel <elizanlilongongopan@gmail.com>
2024-04-19 11:46:38 +0700

    fix: fixing status when sending validation

--- Routes/Sales/index.js
+++ Routes/Sales/index.js
@@ -1,25 +1,33 @@
-const express = require("express");
+const express = require('express');
 const routes = express.Router();
 
 // Controllers
-const SalesController = require("../../Controllers/Sales/sales");
+const SalesController = require('../../Controllers/Sales/sales');
 
 // Middlewares
-const SalesMiddleware = require("../../MiddleWares/Sales/salesValidation");
-const JWTMiddleware = require("../../MiddleWares/Auth/Authorization");
+const SalesMiddleware = require('../../MiddleWares/Sales/salesValidation');
+const JWTMiddleware = require('../../MiddleWares/Auth/Authorization');
 
 routes.get(
-  "/",
+  '/',
   [JWTMiddleware.verifyToken, JWTMiddleware.verifyJWTToken],
   SalesController.GetSales
 );
 routes.post(
-  "/",
+  '/',
   [JWTMiddleware.verifyToken, JWTMiddleware.verifyJWTToken],
   SalesMiddleware.salesValidation,
   SalesController.CreateSales
 );
-routes.put("/:id", SalesController.UpdateSales);
-routes.delete("/:id", SalesController.DeleteSales);
+
+routes.post(
+  '/payment',
+  [JWTMiddleware.verifyToken, JWTMiddleware.verifyJWTToken],
+  SalesMiddleware.salesValidation,
+  SalesController.Payment
+);
+
+routes.put('/:id', SalesController.UpdateSales);
+routes.delete('/:id', SalesController.DeleteSales);
 
 module.exports = routes;
